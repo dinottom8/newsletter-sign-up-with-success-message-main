e1c0517a9241368a2fa02e48cccea8e0caa16824
Mateus Dinotto <dinottom8@gmail.com>
2023-05-28 19:09:08 -0300

    feito 50% js

---
+++ app.js
@@ -0,0 +1,14 @@
+var inputEmail = (document.getElementById('inputEmail')).value;
+var inputButton = document.getElementById('inputButton');
+
+inputButton.addEventListener('onclick', verificar);
+
+function verificar () {
+    if (inputEmail == '') {
+        document.getElementById("inputEmail").style.border = '1pt solid hsl(4, 100%, 67%)';
+        document.getElementById("inputEmail").style.backgroundColor = 'hsl(4, 100%, 67%, 0.2)';        
+        document.getElementById("mensagemErro").style.display = 'inline'
+    } else {
+        window.alert("Tudo certo!");
+    }
+}

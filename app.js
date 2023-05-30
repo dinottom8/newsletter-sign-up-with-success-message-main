c3136267127595afc6e5971c1442cde1f2dc6f1e
Mateus Dinotto <dinottom8@gmail.com>
2023-05-29 21:48:00 -0300

    feitojs

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
-var inputEmail = (document.getElementById('inputEmail')).value;
-var inputButton = document.getElementById('inputButton');
+function chamarPag2 () {
+    document.getElementById("main").style.display = "none";
+    document.getElementById("mainSecondPage").style.display = "inline";
+}
 
-inputButton.addEventListener('onclick', verificar);
-
-function verificar () {
-    if (inputEmail == '') {
-        document.getElementById("inputEmail").style.border = '1pt solid hsl(4, 100%, 67%)';
-        document.getElementById("inputEmail").style.backgroundColor = 'hsl(4, 100%, 67%, 0.2)';        
-        document.getElementById("mensagemErro").style.display = 'inline'
-    } else {
-        document.getElementById("main").style.display = 'none';
-    }
+function chamarPag1 () {
+    document.getElementById("main").style.display = "flex";
+    document.getElementById("mainSecondPage").style.display = "none";
 }
